e9d8c6c25b1a6318387cea740bbacfb360f4f868
Ayush Singh <akhileshayush.2016@gmail.com>
2024-07-22 15:54:46 +0530

    Time: 12 ms (36.33%), Space: 3.3 MB (26.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.rs
+++ 0053-maximum-subarray/0053-maximum-subarray.rs
@@ -4,11 +4,10 @@ impl Solution {
         let mut max_sum = nums[0];
         let n = nums.len();
         for i in 1..n {
-            if curr_sum > 0 {
-                curr_sum = curr_sum + nums[i];
-            } else {
-                curr_sum = nums[i];
-            }
+            if curr_sum <= 0 {
+                curr_sum = 0;
+            } 
+            curr_sum = curr_sum + nums[i];
             if max_sum < curr_sum {
                 max_sum = curr_sum;
             }
