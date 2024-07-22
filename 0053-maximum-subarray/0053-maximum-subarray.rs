1457cf8413182ecfd7d034cf5dad6c937447e4ae
Ayush Singh <akhileshayush.2016@gmail.com>
2024-07-22 15:48:33 +0530

    Time: 11 ms (53.06%), Space: 3.2 MB (26.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.rs
+++ 0053-maximum-subarray/0053-maximum-subarray.rs
@@ -2,7 +2,8 @@ impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         let mut curr_sum = nums[0];
         let mut max_sum = nums[0];
-        for i in 1..nums.len() {
+        let n = nums.len();
+        for i in 1..n {
             curr_sum = std::cmp::max(curr_sum + nums[i], nums[i]);
             max_sum = std::cmp::max(curr_sum, max_sum);
         }
